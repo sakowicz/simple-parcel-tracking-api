7f9f1460c2e2f5fb174770a1f6d5ec3055419c9d
Szymon Sakowicz <giebeka@gmail.com>
2018-12-05 12:31:08 +0100

    cleaning up

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Parcel;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index() {
-        $parcels = Parcel::where('id', '!=', 404)->get();
+        $parcels = DB::table('parcels')->get();
         return view('home', compact('parcels'));
     }
 }
